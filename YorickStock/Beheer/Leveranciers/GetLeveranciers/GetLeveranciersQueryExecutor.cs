f7cefa78e184dacc13b0317b2a21562614e5ba6c
W0dan <W0dan.git@gmail.com>
2013-04-30 22:04:04 +0200

    verbetering cont'd

--- YorickStock/Beheer/Leveranciers/GetLeveranciers/GetLeveranciersQueryExecutor.cs
+++ YorickStock/Beheer/Leveranciers/GetLeveranciers/GetLeveranciersQueryExecutor.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Text;
 using SamStock.Database;
 
-namespace SamStock.Beheer.Leveranciers.GetLeveranciers {
-    public class GetLeveranciersQueryExecutor : IGetLeveranciersQueryExecutor {
+namespace SamStock.Beheer.Leveranciers.GetLeveranciers
+{
+    public class GetLeveranciersQueryExecutor : IGetLeveranciersQueryExecutor
+    {
         private readonly IContext _context;
 
-        public GetLeveranciersQueryExecutor(IContext context) {
+        public GetLeveranciersQueryExecutor(IContext context)
+        {
             _context = context;
         }
 
-        public GetLeveranciersResponse Execute(GetLeveranciersRequest request) {
-            var result = _context.Component
-                .Select(x => new GetLeveranciersItem {
-                    Naam = x.Naam
+        public GetLeveranciersResponse Execute(GetLeveranciersRequest request)
+        {
+            //Opmerking: als je leveranciers wil opzoeken, moet je niet querieën in de Componenten tabel ;-)
+            var result = _context.Leverancier
+                .Select(x => new GetLeveranciersItem
+                {
+                    Naam = x.Naam,
+                    //Opmerking: alle velden invullen
+                    Adres = x.Adres,
+                    Website = x.Site
                 })
                 .ToList();
 
