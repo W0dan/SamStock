1b1bcf0cce4a976c75dcaacfc887fca40d5c2c9f
Marnick L'Eau <marnick.leau@skynet.be>
2013-05-29 19:42:37 +0200

    Added SQL script for orders

---
+++ SamStock.Web/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SamStock.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
