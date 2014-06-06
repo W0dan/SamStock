bf4addc808f04dd81516359905cff8f1dc4f135f
TheRealFaziri <marnick.leau@skynet.be>
2014-06-06 21:49:57 +0200

    Git test 2

---
+++ SAMStock/DAL/Components/Create/CreateComponentExecutor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAMStock.DAL.Base;
+using SAMStock.Database;
+
+namespace SAMStock.DAL.Components.Create
+{
+	public class CreateComponentExecutor: CommandExecutor<CreateComponentCommand>
+	{
+		public CreateComponentExecutor(IContext context) : base(context)
+		{
+		}
+
+		public override int Execute(CreateComponentCommand cmd)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SAMStock/DAL/Components/Create/CreateComponentHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAMStock.DAL.Base;
+
+namespace SAMStock.DAL.Components.Create
+{
+	public class CreateComponentHandler: CommandHandler<CreateComponentCommand>
+	{
+		public CreateComponentHandler(ICommandExecutor<CreateComponentCommand> executor) : base(executor)
+		{
+		}
+	}
+}
